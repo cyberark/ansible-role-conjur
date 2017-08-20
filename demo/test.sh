6aeb07c817e8e95d6213e05f0625c5a8c6d8dcbe
Oren Ben Meir <oren.benmeir@cyberark.com>
2017-08-20 12:33:24 +0300

    Passing HFTOKEN to ansible-runner as environment variable

--- demo/test.sh
+++ demo/test.sh
@@ -17,7 +17,7 @@ function createTestEnvironment() {
   echo "Creating test environment"
   echo '-----'
 
-#	docker-compose pull postgres conjur client todo: uncomment
+	docker-compose pull postgres conjur client todo: uncomment
 	docker-compose build --pull ansible
 	docker-compose up -d client postgres conjur ansible
 
@@ -31,27 +31,16 @@ function createHostFactoryToken() {
   echo "Creating Conjur host factory token"
   echo '-----'
 
-	client_cid=$(docker-compose ps -q client)
-
-	# copy test-policy into a /tmp/test-policy within the client container
-  docker cp test-policy ${client_cid}:/test-policy
-
   # get the api key from conjur for login into Conjur
 	api_key=$(docker-compose exec -T conjur rails r "print Credentials['cucumber:user:admin'].api_key")
 
 	docker exec -e CONJUR_AUTHN_API_KEY=${api_key} \
-		${client_cid} \
-    /bin/bash -c "conjur policy load root /test-policy/root.yml && \
+		ansible-conjur-client \
+    /bin/bash -c "conjur policy load root /conjurinc/ansible/test-policy/root.yml && \
     	conjur variable values add password mysecretpassword && \
 			export HFTOKEN=\$(conjur hostfactory tokens create --duration-days=365 ansible-factory | jq '.[0].token') && \
-			cat >conjurinc/ansible/output/output.sh << OUTPUT
-#!/usr/bin/env bash
-# Run the following in ansible-conjur-role...
-export HFTOKEN=\$HFTOKEN
-#vagrant up
-# Now add the vagrant user to the conjur group and SSH into the vagrant machine.
-#vagrant ssh -c \"sudo usermod -a -G conjur vagrant\" && vagrant ssh
-#conjur variable value password
+			cat >conjurinc/ansible/output/hftoken.txt << OUTPUT
+\$HFTOKEN
 OUTPUT"
 
 		# The command above is untabbed to avoid here-document EOL
@@ -59,10 +48,8 @@ OUTPUT"
 
 function runAnsible() {
 
-	ansible_cid=$(docker-compose ps -q ansible)
-
-	docker cp output ${ansible_cid}:/tmp
-	docker exec -it ${ansible_cid} /bin/bash
+	docker exec -e HFTOKEN=$(<client-output/hftoken.txt) \
+		-it ansible-runner /bin/bash
 }
 
 main
