f7cabf43fbeee51c4eddfde00f2e93912ed494e8
Oren Ben Meir <oren.benmeir@cyberark.com>
2017-08-21 19:08:11 +0300

    Changed tabs to spaces

--- demo/run.sh
+++ demo/run.sh
@@ -1,87 +1,87 @@
 #!/usr/bin/env bash
 
 function finish {
-	echo 'Removing demo environment'
-	echo '-----'
-	docker-compose down -v
+  echo 'Removing demo environment'
+  echo '-----'
+  docker-compose down -v
 }
 trap finish EXIT
 
 function main() {
-	echo '-----'
-	echo '----------'
-	echo '---------------'
-	echo "Starting Ansible demo for Conjur"
-	echo '---------------'
-	echo '----------'
-	echo '-----'
+  echo '-----'
+  echo '----------'
+  echo '---------------'
+  echo "Starting Ansible demo for Conjur"
+  echo '---------------'
+  echo '----------'
+  echo '-----'
 
-	createDemoEnvironment
-	waitForServer
-	createHostFactoryToken
-	runAnsible
-	RetrieveSecretInTarget
+  createDemoEnvironment
+  waitForServer
+  createHostFactoryToken
+  runAnsible
+  RetrieveSecretInTarget
 }
 
 function createDemoEnvironment() {
-	echo "Creating demo environment"
-	echo '-----'
+  echo "Creating demo environment"
+  echo '-----'
 
-	docker-compose build --pull target
-	docker-compose pull postgres conjur client
-	docker-compose up -d client postgres conjur target
+  docker-compose build --pull target
+  docker-compose pull postgres conjur client
+  docker-compose up -d client postgres conjur target
 
-	echo '-----'
+  echo '-----'
 }
 
 function waitForServer() {
-	echo 'Waiting for conjur server to be healthy'
-	echo '-----'
+  echo 'Waiting for conjur server to be healthy'
+  echo '-----'
 
-	# TODO: remove this once we have HEALTHCHECK in place
-	docker-compose run \
-		--entrypoint bash \
-		client \
-		/conjurinc/ansible/wait_for_server.sh
+  # TODO: remove this once we have HEALTHCHECK in place
+  docker-compose run \
+    --entrypoint bash \
+    client \
+    /conjurinc/ansible/wait_for_server.sh
 
-	echo '-----'
+  echo '-----'
 }
 
 function createHostFactoryToken() {
-	echo "Creating Conjur host factory token"
-	echo '-----'
+  echo "Creating Conjur host factory token"
+  echo '-----'
 
-	# get the api key from conjur for login into Conjur
-	api_key=$(docker-compose exec -T conjur rails r "print Credentials['cucumber:user:admin'].api_key")
+  # get the api key from conjur for login into Conjur
+  api_key=$(docker-compose exec -T conjur rails r "print Credentials['cucumber:user:admin'].api_key")
 
-	docker-compose run \
-		-e CONJUR_AUTHN_API_KEY=${api_key} \
-		--entrypoint bash \
-		client \
-		/conjurinc/ansible/create_host_token.sh
+  docker-compose run \
+    -e CONJUR_AUTHN_API_KEY=${api_key} \
+    --entrypoint bash \
+    client \
+    /conjurinc/ansible/create_host_token.sh
 
-	echo '-----'
+  echo '-----'
 }
 
 function runAnsible() {
-	echo "Running Ansible playbook"
-	echo '-----'
+  echo "Running Ansible playbook"
+  echo '-----'
 
-	export HFTOKEN=$(<client-output/hftoken.txt)
+  export HFTOKEN=$(<client-output/hftoken.txt)
 
-	ansible-playbook demo.yml
+  ansible-playbook demo.yml
 
-	echo '-----'
+  echo '-----'
 }
 
 function RetrieveSecretInTarget() {
-	echo "Fetching secret in target container"
-	echo '-----'
+  echo "Fetching secret in target container"
+  echo '-----'
 
-	docker exec ansible-target bash -c "echo Conjur identity is: && conjur authn whoami && echo && echo Conjur secret is: \
-		&& conjur variable value password && echo"
+  docker exec ansible-target bash -c "echo Conjur identity is: && conjur authn whoami && echo && echo Conjur secret is: \
+    && conjur variable value password && echo"
 
-	echo '-----'
+  echo '-----'
 }
 
 main
