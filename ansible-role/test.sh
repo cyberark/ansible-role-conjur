f89a9b8f0e9171d4e5683b58e18e1f84fb3d0609
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2017-11-15 22:48:20 +0100

    again ._.

--- ansible-role/test.sh
+++ ansible-role/test.sh
@@ -16,19 +16,19 @@ declare ansible_cid=''
 function api_key {
   if [ -z ${_api_key} ]
   then
-    _api_key=$(docker exec $(conjur_cid) rails r "print Credentials['cucumber:user:admin'].api_key")
+    _api_key=$(docker exec ${conjur_cid} rails r "print Credentials['cucumber:user:admin'].api_key")
   fi
   echo ${_api_key}
 }
 
 function hf_token {
-  echo $(docker exec $(cli_cid) env CONJUR_AUTHN_API_KEY=$(api_key) conjur hostfactory tokens create --duration-minutes=5 ansible/ansible-factory | jq -r '.[0].token')
+  echo $(docker exec ${cli_cid} env CONJUR_AUTHN_API_KEY=$(api_key) conjur hostfactory tokens create --duration-minutes=5 ansible/ansible-factory | jq -r '.[0].token')
 }
 
 function setup_conjur {
   echo "---- setting up conjur ----"
   # run policy
-  docker exec $(cli_cid) env CONJUR_AUTHN_API_KEY=$(api_key) conjur policy load root /policy/root.yml
+  docker exec ${cli_cid} env CONJUR_AUTHN_API_KEY=$(api_key) conjur policy load root /policy/root.yml
 
   # set secret values
   local set_secrets_commands=$(cat <<-EOF
@@ -37,7 +37,7 @@ conjur variable values add ansible/another-target-password another_target_secret
 conjur variable values add ansible/master-password ansible_master_secret_password
 EOF
 )
-  docker exec $(cli_cid) env CONJUR_AUTHN_API_KEY=$(api_key) bash -c "$set_secrets_commands"
+  docker exec ${cli_cid} env CONJUR_AUTHN_API_KEY=$(api_key) bash -c "$set_secrets_commands"
 }
 
 function run_test_cases {
@@ -52,8 +52,8 @@ function run_test_case {
   local test_case=$1
   if [ ! -z "$test_case" ]
   then
-    docker exec $(ansible_cid) env HFTOKEN=$(hf_token) bash -c "cd ansible-role && ansible-playbook test_cases/${test_case}/playbook.yml"
-    docker exec $(ansible_cid) bash -c "cd ansible-role && py.test --junitxml=./junit/${test_case} --connection docker -v test_cases/${test_case}/tests/test_default.py"
+    docker exec ${ansible_cid} env HFTOKEN=$(hf_token) bash -c "cd ansible-role && ansible-playbook test_cases/${test_case}/playbook.yml"
+    docker exec ${ansible_cid} bash -c "cd ansible-role && py.test --junitxml=./junit/${test_case} --connection docker -v test_cases/${test_case}/tests/test_default.py"
   else
     echo ERROR: run_test called with no argument 1>&2
     exit 1
@@ -73,7 +73,7 @@ done
 EOF
 )
 function wait_for_server {
-  docker exec $(cli_cid) bash -c "$wait_for_server_command"
+  docker exec ${cli_cid} bash -c "$wait_for_server_command"
 }
 
 function main() {
