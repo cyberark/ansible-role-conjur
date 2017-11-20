ce4197f3f02b48f8e2d5e1a8a9ca865aa1078e92
Jason Vanderhoof <jvanderhoof@gmail.com>
2017-11-20 07:38:32 -0700

    Removed folder that is no longer required

--- test-files/fetch_cert.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd /conjurinc/ansible
-mkdir -p output
-rm -f output/conjur.pem
-rm -f /root/conjur-cucumber.pem
-rm -f /root/.conjurrc
-echo "yes" | conjur init -u "${CONJUR_PROXY_URL}" -a "${CONJUR_ACCOUNT}"
-cp /root/conjur-cucumber.pem output/conjur.pem
