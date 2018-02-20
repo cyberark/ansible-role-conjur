9a921109be7aac7d013c445518e4e60b1dd3237f
Jason Vanderhoof <jvanderhoof@gmail.com>
2018-02-20 14:46:37 -0700

    Use installed Summon & Summon-Conjur for test verification.

--- tests/test_cases/configure-conjur-identity/tests/test_default.py
+++ tests/test_cases/configure-conjur-identity/tests/test_default.py
@@ -23,20 +23,7 @@ def test_is_conjurized(host):
     assert conf_file.user == 'root'
 
 
-def test_whoami(host):
-    result = host.check_output("conjur authn whoami", shell=True)
-
-    assert result == \
-        "{\"account\":\"cucumber\"," \
-        "\"username\":\"host/conjur_%s\"}" % host.check_output('hostname')
-
-
-def test_retrieve_secret_with_cli(host):
-    result = host.check_output("conjur variable value ansible/target-password", shell=True)
-
-    assert result == "target_secret_password"
-
 def test_retrieve_secret_with_summon(host):
-    result = host.check_output("echo 'secret: !var ansible/target-password' >> secrets.yml; summon bash -c 'echo $secret'", shell=True)
+    result = host.check_output("summon --yaml 'DB_USERNAME: !var ansible/target-password' bash -c 'printenv DB_USERNAME'", shell=True)
 
     assert result == "target_secret_password"
